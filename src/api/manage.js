33bf292d8d37162abb490269eedb53c7fa8c8fd2
zhuxing-wang <1124818036@qq.com>
2025-04-23 15:10:56 +0800

    feat: 添加社区板块的接口

--- src/api/manage.js
+++ src/api/manage.js
@@ -9,7 +9,14 @@ const api = {
   orgTree: '/org/tree',
   uploadavatar: 'http://localhost:8080/account/UploadAvatar',
   getwordbook: 'http://localhost:8080/api/wordbooks',
-  addwordbook: 'http://localhost:8080/api/wordbooks/addWordbook'
+  addwordbook: 'http://localhost:8080/api/wordbooks/addWordbook',
+  getpostcount: 'http://localhost:8080/api/admin/getpostcount',
+  deletepost: 'http://localhost:8080/api/admin/deletepost',
+  searchposttitle: 'http://localhost:8080/api/admin/searchposttitle',
+  searchpostusername: 'http://localhost:8080/api/admin/searchpostusername',
+  searchpostuserid: 'http://localhost:8080/api/admin/searchpostuserId',
+  searchpoststate: 'http://localhost:8080/api/admin/searchpoststate',
+  getuserscount: 'http://localhost:8080/api/admin/getuserscount'
 }
 
 export default api
@@ -123,5 +130,123 @@ export function saveSub (sub) {
     url: '/sub',
     method: sub.id === 0 ? 'post' : 'put',
     data: sub
+  })
+}
+
+export function getpostcount () {
+  return request({
+    url: api.getpostcount,
+    method: 'get'
+  })
+}
+
+export function getpost (id) {
+  return request({
+    url: `http://localhost:8080/api/getpost/${id}`,
+    method: 'get'
+  })
+}
+
+export function deletepost (id, message) {
+  return request({
+    url: api.deletepost,
+    method: 'post',
+    data: {
+      id,
+      message
+    }
+  })
+}
+
+export function getsearchposttitle (keyword) {
+  return request({
+    url: api.searchposttitle,
+    method: 'get',
+    data: {
+      keyword
+    }
+  })
+}
+
+export function getsearchpostusername (username) {
+  return request({
+    url: api.searchpostusername,
+    method: 'get',
+    data: {
+      username
+    }
+  })
+}
+
+export function getsearchpostuserId (userId) {
+  return request({
+    url: api.searchpostuserId,
+    method: 'get',
+    data: {
+      userId
+    }
+  })
+}
+
+export function getsearchpoststate (state) {
+  return request({
+    url: api.searchpoststate,
+    method: 'get',
+    data: {
+      state
+    }
+  })
+}
+
+export function postsetstate (id, state) {
+  return request({
+    url: `http://localhost:8080/api/admin/setstate/${id}`,
+    method: 'post',
+    data: {
+      state
+    }
+  })
+}
+
+export function getpostsid (id) {
+  return request({
+    url: `http://localhost:8080/api/admin/posts/${id}`,
+    method: 'post'
+  })
+}
+
+export function deletepostidcomment (id, commentId, message) {
+  return request({
+    url: `http://localhost:8080/api/admin/posts/${id}`,
+    method: 'post',
+    data: {
+      commentId,
+      message
+    }
+  })
+}
+
+export function getuserscount () {
+  return request({
+    url: api.getuserscount,
+    method: 'get'
+  })
+}
+
+export function getuser (id) {
+  return request({
+    url: `http://localhost:8080/api/admin/getuser/${id}`,
+    method: 'get'
+  })
+}
+
+export function postusersetstate (id, state, message) {
+  return request({
+    url: `http://localhost:8080/api/admin/users/${id}/setstate`,
+    method: 'post',
+    data: {
+      state,
+      message
+    }
   })
 }
